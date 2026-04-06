1ac6b9a44b606010fba98e4a4e7ce3b67bca8f1e
Ibrahimfaqath <ibrahimfaqath@gmail.com>
2026-04-06 11:03:02 +0700

    add latihan1.js "

---
+++ latihan1.js
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const schema = z.object({
+    name: z.string(),
+    age: z.number(),
+});
+
+const result = schema.safeParse({
+    name: "Aris",
+    age: 25,
+});
+
+if (!result.success) {
+    console.log(result.error.format());
+}
+
+console.log(schema.safeParse({
+    name: "Aris",
+    age: 25,
+}));
